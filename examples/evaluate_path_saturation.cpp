b0e81088dbd93bf42edc0eab59fdd3c397c45849
Brandon Apodaca <bapodaca@umich.edu>
2024-12-05 16:47:05 -0500

    update station oriented saturation values

--- examples/evaluate_path_saturation.cpp
+++ examples/evaluate_path_saturation.cpp
@@ -30,10 +30,10 @@ int main(int argc, char** argv) {
         // "cw_opt_packaged_50",
         // "cw_opt_packaged_var",
         // "packaged_paths_ko/pf_final",
-        "packaged_paths_ko_slerp/pf_final",
-        "packaged_paths_ma/pf_final",
-        "packaged_paths_ma_slerp/pf_final"
-        // "packaged_paths_so/pf_final",
+        // "packaged_paths_ko_slerp/pf_final",
+        // "packaged_paths_ma/pf_final",
+        // "packaged_paths_ma_slerp/pf_final"
+        "packaged_paths_so/pf_final"
     };
 
     std::vector<std::string> savefolders = {
@@ -41,10 +41,10 @@ int main(int argc, char** argv) {
         // "ivt_50",
         // "ivt_var",
         // "ocp_ko",
-        "ocp_ko_slerp",
-        "ocp_ma",
-        "ocp_ma_slerp"
-        // "ocp_so"
+        // "ocp_ko_slerp",
+        // "ocp_ma",
+        // "ocp_ma_slerp"
+        "ocp_so"
     };
 
     std::vector<std::string> conditions = {
